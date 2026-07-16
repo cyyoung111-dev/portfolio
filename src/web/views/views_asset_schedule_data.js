199e2de07453bbf9e43a1149b66716553a705562
cyyoung111-dev <cyyoung111@gmail.com>
2026-07-16 09:49:42 +0900

    Add files via upload

--- src/web/views/views_asset_schedule_data.js
+++ src/web/views/views_asset_schedule_data.js
@@ -226,8 +226,6 @@ function removeReValue(sortedIdx) {
 
 // ★ [통일] data-schedule-action 클릭 처리는 event_delegation.js로 통합됨
 
-document.addEventListener('change', function(e) {
-  if (e.target.dataset?.scheduleFile === 'upload') uploadScheduleCsv(e.target);
-});
+// ★ [통일] scheduleFile 업로드 change 처리도 event_delegation.js로 통합됨
 
 // ★ [통일] number-comma 입력 서식 처리는 event_delegation.js의 전역 input 리스너로 통합됨
